5606cb0b03bcb4afd3198f3c39c037e4f52d7dfb
liuw518 <liu.w518@gmail.com>
2017-02-07 01:39:48 +0800

    在Express中集成log4js的中间件

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
@@ -10,13 +9,17 @@ var users = require('./routes/users');
 
 var app = express();
 
+var log4js = require('log4js');
+log4js.configure(require('./log4js.json'));
+var logger = log4js.getLogger('console');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(log4js.connectLogger(logger, {level:'auto'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -37,7 +40,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
+  logger.error(err);
   // render the error page
   res.status(err.status || 500);
   res.render('error');
