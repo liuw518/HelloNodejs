5f7e6b6e93a6ea7a3059ead0dc0e1b9074a19719
liuw518 <liu.w518@gmail.com>
2017-02-09 17:37:23 +0800

    添加passport测试代码

--- app/app.js
+++ app/app.js
@@ -3,10 +3,11 @@ var path = require('path');
 // var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routersLoader = require('routers-loader');
 var logFactory = require('logfactory');
 var logger = logFactory.getLogger();
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
@@ -24,7 +25,38 @@ app.use(logFactory.log4js.connectLogger(logger, {level: 'auto'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(express.session({secret: 'blog.fens.me', cookie: { maxAge: 60000 }}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+
+passport.use('local', new LocalStrategy(
+    function (username, password, done) {
+        var user = {
+            id: '1',
+            username: 'admin',
+            password: 'pass'
+        }; // 可以配置通过数据库方式读取登陆账号
+
+        if (username !== user.username) {
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (password !== user.password) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+
+        return done(null, user);
+    }
+));
+
+passport.serializeUser(function (user, done) {//保存user对象
+    done(null, user);//可以通过数据库方式操作
+});
+
+passport.deserializeUser(function (user, done) {//删除user对象
+    done(null, user);//可以通过数据库方式操作
+});
+
 
 //dynamic load routers
 routersLoader('./app/routes', app);
