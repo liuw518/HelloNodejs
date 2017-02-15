6a423ee3cf34a5b53aa06b0cb2487b6f90fe89dc
liuw518 <liu.w518@gmail.com>
2017-02-15 14:07:06 +0800

    程序入口改为app，添加http2支持

--- app/app.js
+++ app/app.js
@@ -3,14 +3,17 @@ if(global._){
 } else {
   global._ = require('lodash');
 }
-
+var http = require('http');
+var spdy = require('spdy');
+var fs = require('fs');
 var express = require('express');
 var path = require('path');
 // var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var auth = require('auth');
 
+var config = require('config');
+var auth = require('auth');
 var routersLoader = require('routers-loader');
 var logFactory = require('log-factory');
 var logger = logFactory.getLogger();
@@ -59,4 +62,66 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+var port = normalizePort(process.env.PORT || config.port);
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+  return false;
+}
+app.set('port', port);
+
+var server = null;
+if(config.SSL){
+  var options = {
+    cert: fs.readFileSync(config.SSL.cert),
+    key: fs.readFileSync(config.SSL.key)
+  }
+  server = spdy.createServer(options, app);
+} else {
+  server = http.createServer(app);
+}
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  if(config.SSL){
+    logger.info('HTTPS listening on ' + bind);
+  } else {
+    logger.info('HTTP listening on ' + bind);
+  }
+}
