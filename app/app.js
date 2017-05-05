5fc961898bc561178b603d2dfda2e305d479a252
liuw518 <liu.w518@gmail.com>
2017-05-05 11:02:50 +0800

    调整格式

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 if (global._) {
-    throw new Error('global中已经存在名为_的属性，无法将lodash注册到global._中');
+  throw new Error('global中已经存在名为_的属性，无法将lodash注册到global._中');
 } else {
-    global._ = require('lodash');
+  global._ = require('lodash');
 }
 var http = require('http');
 var spdy = require('spdy');
@@ -21,7 +21,7 @@ var logger = logFactory.getLogger();
 var app = express();
 
 process.on('uncaughtException', function(err) {
-    logger.error(err);
+  logger.error(err);
 });
 
 // view engine setup
@@ -31,11 +31,11 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logFactory.log4js.connectLogger(logger, {
-    level: 'auto'
+  level: 'auto'
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 app.use(cookieParser());
 app.use(express.static(path.join(path.dirname(__dirname), 'public')));
@@ -48,86 +48,86 @@ routersLoader('./app/routes', app, auth.check);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    // var err = new Error('Not Found');
-    // err.status = 404;
-    res.status(404);
-    res.send('找不到指定页面');
-    // next(err);
+  // var err = new Error('Not Found');
+  // err.status = 404;
+  res.status(404);
+  res.send('找不到指定页面');
+  // next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    logger.error(err);
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  logger.error(err);
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 var port = normalizePort(process.env.PORT || config.port);
 
 function normalizePort(val) {
-    var port = parseInt(val, 10);
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-    return false;
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+  return false;
 }
 app.set('port', port);
 
 var server = null;
 if (config.SSL) {
-    var options = {
-        cert: fs.readFileSync(config.SSL.cert),
-        key: fs.readFileSync(config.SSL.key)
-    };
-    server = spdy.createServer(options, app);
+  var options = {
+    cert: fs.readFileSync(config.SSL.cert),
+    key: fs.readFileSync(config.SSL.key)
+  };
+  server = spdy.createServer(options, app);
 } else {
-    server = http.createServer(app);
+  server = http.createServer(app);
 }
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
 function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
 
-    var bind = typeof port === 'string' ?
-        'Pipe ' + port :
-        'Port ' + port;
+  var bind = typeof port === 'string' ?
+    'Pipe ' + port :
+    'Port ' + port;
 
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
     case 'EACCES':
-        console.error(bind + ' requires elevated privileges');
-        process.exit(1);
-        break;
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
     case 'EADDRINUSE':
-        console.error(bind + ' is already in use');
-        process.exit(1);
-        break;
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
     default:
-        throw error;
-    }
+      throw error;
+  }
 }
 
 function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string' ?
-        'pipe ' + addr :
-        'port ' + addr.port;
-    if (config.SSL) {
-        logger.info('HTTPS listening on ' + bind);
-    } else {
-        logger.info('HTTP listening on ' + bind);
-    }
+  var addr = server.address();
+  var bind = typeof addr === 'string' ?
+    'pipe ' + addr :
+    'port ' + addr.port;
+  if (config.SSL) {
+    logger.info('HTTPS listening on ' + bind);
+  } else {
+    logger.info('HTTP listening on ' + bind);
+  }
 }
--- app/routes/demo/db.js
+++ app/routes/demo/db.js
@@ -6,40 +6,40 @@ var dbutil = require('db-helper');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    Promise.using(dbutil.getConnection(), function(conn) {
-        return conn.beginTransactionAsync()
-            .then(function() {
-                return conn.queryAsync('insert into node_user set id = 999');
-            })
-            .then(function(results) {
-                logger.info('第一次查询结束');
-                return {
-                    a: 1,
-                    b: 2
-                };
-            })
-            .then(function(results) {
-                logger.info('第二次请求前接到的参数：' + JSON.stringify(results));
-                return conn.queryAsync('delete FROM node_user where id = 999');
-            })
-            .then(function(results) {
-                // throw new Error();
-                logger.info('第二次查询结束');
-                return null;
-            })
-            .catch(function(err) {
-                // logger.error(err);
-                throw err;
-            })
-            .finally(function() {
-                logger.info('业务逻辑执行完毕');
-            });
-    });
-    res.send('liu.w');
+  Promise.using(dbutil.getConnection(), function(conn) {
+    return conn.beginTransactionAsync()
+      .then(function() {
+        return conn.queryAsync('insert into node_user set id = 999');
+      })
+      .then(function(results) {
+        logger.info('第一次查询结束');
+        return {
+          a: 1,
+          b: 2
+        };
+      })
+      .then(function(results) {
+        logger.info('第二次请求前接到的参数：' + JSON.stringify(results));
+        return conn.queryAsync('delete FROM node_user where id = 999');
+      })
+      .then(function(results) {
+        // throw new Error();
+        logger.info('第二次查询结束');
+        return null;
+      })
+      .catch(function(err) {
+        // logger.error(err);
+        throw err;
+      })
+      .finally(function() {
+        logger.info('业务逻辑执行完毕');
+      });
+  });
+  res.send('liu.w');
 });
 
 router.get('/:id', function(req, res, next) {
-    res.send('liu.w' + req.params['id']);
+  res.send('liu.w' + req.params['id']);
 });
 
 module.exports = router;
