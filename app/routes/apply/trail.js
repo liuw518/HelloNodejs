013f406a034209c3a6da180851ea01a614ceb615
liuw518 <liu.w518@gmail.com>
2017-05-06 10:14:37 +0800

    添加4个伪接口

---
+++ app/routes/apply/trail.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+/* 试算接口 */
+router.post('/', function(req, res, next) {
+  // 幂等性校验
+  // 入口报文落地
+  // 入口报文转换
+  // 请求报文落地
+  // 响应报文落地
+  // 响应报文转换
+  // 返回报文落地
+  console.log(req.body);
+  var fileStr = fs.readFileSync('public/data/2ssfhbw.json', 'utf8');
+  res.send(fileStr);
+});
+
+module.exports = router;
